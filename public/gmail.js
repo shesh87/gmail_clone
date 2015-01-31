f58e43ef16f43825d609970a82cff973280cb1ba
Sheonna Harris <svonharris@gmail.com>
2015-01-31 18:56:34 -0500

    Created hidden buttons to show when email is checked. However, they hide when a differet email is checked; working on fixing that.

--- public/gmail.js
+++ public/gmail.js
@@ -1,4 +1,12 @@
 $( "input:checkbox" ).on('click', function () {
 	$(this).parents('li').toggleClass('js-checked');
-	
+	$('.js-selected, .js-sort').toggle();
+	$('.js-remove, .js-hidden-sort').toggle();
+	// if ("input:checkbox").is(':checked') === true {
+	// 	alert("yes");
+	// } else {
+	// 	console.log("no");
+	// }
 });
+
+
